00007377eff7c0cc16791e21b4980b6259abaf85
Dave Timmers <david.a.timmers@gmail.com>
2017-03-11 17:08:02 -0500

    Project Creation
    VS Project with bootstrap and scripts

---
+++ FindTheBooty/FindTheBooty/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FindTheBooty.Startup))]
+namespace FindTheBooty
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
