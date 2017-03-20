d0ad027f5af0e76fc524d85309230e413f4babf0
Brandon White <bmwqg@live.com>
2017-03-19 22:20:03 -0400

    pushed main layout w/ hamburger menu implemented

--- FindTheBooty/FindTheBooty/App_Start/BundleConfig.cs
+++ FindTheBooty/FindTheBooty/App_Start/BundleConfig.cs
@@ -24,8 +24,8 @@ namespace FindTheBooty
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Header.css"));
+
         }
     }
 }
