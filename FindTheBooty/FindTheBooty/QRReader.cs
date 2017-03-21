fcb3f6f471e5d3e893ad76b84598153d534b404d
Michael Frederick <master16789@gmail.com>
2017-03-21 18:24:21 -0400

    Create QRGenerator

--- FindTheBooty/FindTheBooty/QRReader.cs
+++ FindTheBooty/FindTheBooty/QRReader.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using MessagingToolkit.QRCode.Codec;
+﻿using MessagingToolkit.QRCode.Codec;
 using MessagingToolkit.QRCode.Codec.Data;
+using System;
 
 namespace FindTheBooty
 {
+    /// <summary>
+    /// This is a wrapper class for reading QR codes.
+    /// </summary>
     public class QRReader
     {
         /// <summary>
         /// Takes the input stream of a image file and returns the string contained in the QR image
         /// </summary>
-        /// <param name="input">A PNG file</param>
+        /// <param name="input">A PNG, JPG, or Static GIF file stream</param>
         /// <returns>QR Code String</returns>
         public static string getQRCode(System.IO.Stream input)
         {
