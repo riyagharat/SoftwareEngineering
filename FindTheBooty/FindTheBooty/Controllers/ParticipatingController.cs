1289c8f7184d563aff0643b80c784b38bd63f744
Brandon White <bmwqg@live.com>
2017-03-23 23:13:47 -0400

    created participating controller; --removed shadow from page bg

---
+++ FindTheBooty/FindTheBooty/Controllers/ParticipatingController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FindTheBooty.Controllers
+{
+    public class ParticipatingController : Controller
+    {
+        // GET: Participating
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewHunts()
+        {
+            return View();
+        }
+    }
+}
