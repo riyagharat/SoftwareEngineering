77b8980d9146fbeb5c7650dc3bf3a9863dd6114d
Dave Timmers <david.a.timmers@gmail.com>
2017-04-12 00:52:15 -0400

    Leaderboard Page

---
+++ FindTheBooty/FindTheBooty/Controllers/LeaderboardController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FindTheBooty.Models.GeneratedModels;
+
+namespace FindTheBooty.Controllers
+{
+    public class LeaderboardController : DataController
+    {
+        // GET: Leaderboard Page
+        public ActionResult Index()
+        {
+            return RedirectToAction("Rank");// View(database.users.OrderByDescending(x => x.points).ToList());
+        }
+
+        // GET: Points Leaderboard
+        public ActionResult Points()
+        {
+            return View(database.users.OrderByDescending(x => x.points).ToList());
+        }
+
+        // GET: Rank Leaderboard
+        public ActionResult Rank()
+        {
+            return View(database.users.OrderByDescending(x => x.rank).ToList());
+        }
+
+        // GET: Hunts Leaderboard
+        public ActionResult Hunts()
+        {
+            return View(database.users.OrderByDescending(x => x.num_hunts).ToList());
+        }
+
+        // GET: Treasures Leaderboard
+        public ActionResult Treasures()
+        {
+            return View(database.users.OrderByDescending(x => x.num_treasures).ToList());
+        }
+    }
+}
