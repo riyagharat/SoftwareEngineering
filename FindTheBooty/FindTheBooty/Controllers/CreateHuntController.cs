b7e0073bb84b0788ea160e0ab2a32dfdbe6c9005
JansK <kmjanssen1@gmail.com>
2017-04-20 14:39:32 -0400

    Adding files

---
+++ FindTheBooty/FindTheBooty/Controllers/CreateHuntController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FindTheBooty.Controllers
+{
+    public class CreateHuntController : Controller
+    {
+        // GET: CreateHunt
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
